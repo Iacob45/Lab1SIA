e4c5f7de45f7c9bef334756748db6d29c681ffca
Iacob45 <andrei_delcea@yahoo.com>
2025-04-02 19:00:10 +0300

    Finished Application 1 of HandwrittenDigitRecognition

--- Laborator3_MNIST_Digit_Recognition/HandwrittenDigitRecognition.py
+++ Laborator3_MNIST_Digit_Recognition/HandwrittenDigitRecognition.py
@@ -1,8 +1,9 @@
-import keras
-from keras.datasets import mnist
-import np_utils
-from keras.utils import to_categorical
-from keras import layers
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras import layers
+import numpy as np
 #####################################################################################################################
 #####################################################################################################################
 
@@ -12,16 +13,16 @@ from keras import layers
 def baseline_model(num_pixels, num_classes):
 
     #TODO - Application 1 - Step 6a - Initialize the sequential model
-    model = None   # Modify this
+    model = keras.models.Sequential()
 
     #TODO - Application 1 - Step 6b - Define a hidden dense layer with 8 neurons
-
+    model.add(layers.Dense(8, input_dim=num_pixels, kernel_initializer='normal', activation='relu'))
 
     #TODO - Application 1 - Step 6c - Define the output dense layer
-
+    model.add(layers.Dense(num_classes, kernel_initializer='normal', activation='softmax'))
 
     # TODO - Application 1 - Step 6d - Compile the model
-
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
     return model
 #####################################################################################################################
@@ -34,22 +35,32 @@ def baseline_model(num_pixels, num_classes):
 def trainAndPredictMLP(X_train, Y_train, X_test, Y_test):
 
     #TODO - Application 1 - Step 3 - Reshape the MNIST dataset - Transform the images to 1D vectors of floats (28x28 pixels  to  784 elements)
-
+    num_pixels = X_train.shape[1] * X_train.shape[2]
+    X_train = X_train.reshape(X_train.shape[0], num_pixels).astype('float32')
+    X_test = X_test.reshape(X_test.shape[0], num_pixels).astype('float32')
 
     #TODO - Application 1 - Step 4 - Normalize the input values
+    X_train = X_train / 255
+    X_test = X_test / 255
 
 
     #TODO - Application 1 - Step 5 - Transform the classes labels into a binary matrix
+    Y_train = to_categorical(Y_train)
+    Y_test = to_categorical(Y_test)
+    num_classes = Y_test.shape[1]
+
 
 
     #TODO - Application 1 - Step 6 - Build the model architecture - Call the baseline_model function
-    model = None   #Modify this
+    model = baseline_model(num_pixels, num_classes)
 
 
     #TODO - Application 1 - Step 7 - Train the model
-
+    model.fit(X_train, Y_train, validation_data=(X_test, Y_test), epochs=10, batch_size=200, verbose=2)
 
     #TODO - Application 1 - Step 8 - System evaluation - compute and display the prediction error
+    scores = model.evaluate(X_test, Y_test, verbose=0)
+    print("Baseline Error: {:.2f}".format(100 - scores[1] * 100))
 
 
     return
@@ -115,6 +126,7 @@ def trainAndPredictCNN(X_train, Y_train, X_test, Y_test):
     #TODO - Application 2 - Step 8 - Final evaluation of the model - compute and display the prediction error
 
 
+
     return
 #####################################################################################################################
 #####################################################################################################################
@@ -125,12 +137,15 @@ def trainAndPredictCNN(X_train, Y_train, X_test, Y_test):
 def main():
 
     #TODO - Application 1 - Step 1 - Load the MNIST dataset in Keras
+    (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
 
 
     #TODO - Application 1 - Step 2 - Train and predict on a MLP - Call the trainAndPredictMLP function
+    trainAndPredictMLP(X_train, Y_train, X_test, Y_test)
 
 
     #TODO - Application 2 - Step 1 - Train and predict on a CNN - Call the trainAndPredictCNN function
+    trainAndPredictCNN(X_train, Y_train, X_test, Y_test)
 
 
     return
