3f0a560259d1d9c939e99205c02adfd194ffc345
Iacob45 <andrei_delcea@yahoo.com>
2025-04-30 06:06:22 +0300

    Completed CNN projects on single digit recognition and clothing classification

--- Laborator3_MNIST_Digit_Recognition/HandwrittenDigitRecognition.py
+++ Laborator3_MNIST_Digit_Recognition/HandwrittenDigitRecognition.py
@@ -7,6 +7,10 @@ import numpy as np
 #####################################################################################################################
 #####################################################################################################################
 
+print("GPU available:", tf.config.list_physical_devices('GPU'))
+import torch
+cudnn_version = torch.backends.cudnn.version()
+print("CuDNN version:", cudnn_version)
 
 #####################################################################################################################
 #####################################################################################################################
@@ -16,10 +20,10 @@ def baseline_model(num_pixels, num_classes):
     model = keras.models.Sequential()
 
     #TODO - Application 1 - Step 6b - Define a hidden dense layer with 8 neurons
-    model.add(layers.Dense(8, input_dim=num_pixels, kernel_initializer='normal', activation='relu'))
+    model.add(layers.Dense(units=16, input_dim=num_pixels, kernel_initializer='normal', activation='relu'))
 
     #TODO - Application 1 - Step 6c - Define the output dense layer
-    model.add(layers.Dense(num_classes, kernel_initializer='normal', activation='softmax'))
+    model.add(layers.Dense(units=num_classes, kernel_initializer='normal', activation='softmax'))
 
     # TODO - Application 1 - Step 6d - Compile the model
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
@@ -50,7 +54,6 @@ def trainAndPredictMLP(X_train, Y_train, X_test, Y_test):
     num_classes = Y_test.shape[1]
 
 
-
     #TODO - Application 1 - Step 6 - Build the model architecture - Call the baseline_model function
     model = baseline_model(num_pixels, num_classes)
 
@@ -73,29 +76,60 @@ def trainAndPredictMLP(X_train, Y_train, X_test, Y_test):
 def CNN_model(input_shape, num_classes):
 
     # TODO - Application 2 - Step 5a - Initialize the sequential model
-    model = None   #Modify this
-
+    model = keras.models.Sequential()
 
     #TODO - Application 2 - Step 5b - Create the first hidden layer as a convolutional layer
-
+    model.add(layers.Conv2D(filters=9, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
 
     #TODO - Application 2 - Step 5c - Define the pooling layer
-
+    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
 
     #TODO - Application 2 - Step 5d - Define the Dropout layer
-
+    model.add(layers.Dropout(rate=0.2))  # 20% dropout
 
     #TODO - Application 2 - Step 5e - Define the flatten layer
-
+    model.add(layers.Flatten())
 
     #TODO - Application 2 - Step 5f - Define a dense layer of size 128
-
+    model.add(layers.Dense(units=128, kernel_initializer='normal', activation='relu'))
 
     #TODO - Application 2 - Step 5g - Define the output layer
-
+    model.add(layers.Dense(units=num_classes, kernel_initializer='normal', activation='softmax'))
 
     #TODO - Application 2 - Step 5h - Compile the model
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+    return model
+
+def CNN_model2(input_shape, num_classes):
+
+    # TODO - Application 2 - Step 5a - Initialize the sequential model
+    model = keras.models.Sequential()
+
+    #TODO - Application 2 - Step 5b - Create the first hidden layer as a convolutional layer
+    model.add(layers.Conv2D(filters=30, kernel_size=(5, 5), activation='relu', input_shape=input_shape))
+
+    #TODO - Application 2 - Step 5c - Define the pooling layer
+    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(layers.Conv2D(filters=15, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+
+    #TODO - Application 2 - Step 5d - Define the Dropout layer
+    model.add(layers.Dropout(rate=0.2))  # 20% dropout
+
+    #TODO - Application 2 - Step 5e - Define the flatten layer
+    model.add(layers.Flatten())
+
+    #TODO - Application 2 - Step 5f - Define a dense layer of size 128
+    model.add(layers.Dense(units=128, kernel_initializer='normal', activation='relu'))
+    #model.add(layers.Dense(units=50, kernel_initializer='normal', activation='relu'))
+
+    #TODO - Application 2 - Step 5g - Define the output layer
+    model.add(layers.Dense(units=num_classes, kernel_initializer='normal', activation='softmax'))
+
+    #TODO - Application 2 - Step 5h - Compile the model
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
     return model
 #####################################################################################################################
@@ -108,23 +142,28 @@ def CNN_model(input_shape, num_classes):
 def trainAndPredictCNN(X_train, Y_train, X_test, Y_test):
 
     #TODO - Application 2 - Step 2 - reshape the data to be of size [samples][width][height][channels]
-
+    X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], X_train.shape[1], 1).astype('float32')
+    X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], X_test.shape[1], 1).astype('float32')
 
     #TODO - Application 2 - Step 3 - normalize the input values from 0-255 to 0-1
-
+    X_train /= 255
+    X_test /= 255
 
     #TODO - Application 2 - Step 4 - One hot encoding - Transform the classes labels into a binary matrix
-
+    Y_train = to_categorical(Y_train)
+    Y_test = to_categorical(Y_test)
+    num_classes = Y_train.shape[1]
 
     #TODO - Application 2 - Step 5 - Call the cnn_model function
-    model = None   #Modify this
+    model = CNN_model2(X_train.shape[1:],num_classes)
 
 
     #TODO - Application 2 - Step 6 - Train the model
-
+    model.fit(x=X_train, y=Y_train, batch_size=200, epochs=20, verbose=2, validation_data=(X_test, Y_test), shuffle=True)
 
     #TODO - Application 2 - Step 8 - Final evaluation of the model - compute and display the prediction error
-
+    scores = model.evaluate(x=X_test, y=Y_test, verbose=0)
+    print("CNN Error: {:.2f}".format(100 - scores[1] * 100))
 
 
     return
@@ -141,8 +180,7 @@ def main():
 
 
     #TODO - Application 1 - Step 2 - Train and predict on a MLP - Call the trainAndPredictMLP function
-    trainAndPredictMLP(X_train, Y_train, X_test, Y_test)
-
+    #trainAndPredictMLP(X_train, Y_train, X_test, Y_test)
 
     #TODO - Application 2 - Step 1 - Train and predict on a CNN - Call the trainAndPredictCNN function
     trainAndPredictCNN(X_train, Y_train, X_test, Y_test)
--- Laborator4_MNIST_Fashion_Classification/FashionRecognition.py
+++ Laborator4_MNIST_Fashion_Classification/FashionRecognition.py
@@ -47,8 +47,8 @@ def prepareData(trainX, trainY, testX, testY):
     testX = testX.reshape(testX.shape[0], trainX.shape[1], trainX.shape[2], 1).astype('float32')
 
     #TODO - Application 1 - Step 4b - normalize the input values
-    trainX = trainX/255
-    testX= testX/255
+    trainX /= 255
+    testX /= 255
 
     #TODO - Application 1 - Step 4c - Transform the classes labels into a binary matrix
     trainY = to_categorical(trainY)
@@ -71,21 +71,24 @@ def defineModel(input_shape, num_classes):
     #TODO - Application 1 - Step 6b - Create the first hidden layer as a convolutional layer
     model.add(layers.Input(shape=input_shape))
     model.add(layers.Convolution2D(kernel_size=(3, 3), filters=32, kernel_initializer='he_uniform', activation='relu'))
+    #, padding='same'
 
     #TODO - Application 1 - Step 6c - Define the pooling layer
-    model.add(layers.MaxPooling2D())
+    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(layers.Dropout(rate=0.2))
 
     #TODO - Application 1 - Step 6d - Define the flatten layer
     model.add(layers.Flatten())
 
     #TODO - Application 1 - Step 6e - Define a dense layer of size 16
-    model.add(layers.Dense(16, kernel_initializer='he_uniform', activation='relu'))
+    model.add(layers.Dense(units=16, kernel_initializer='he_uniform', activation='relu'))
 
     #TODO - Application 1 - Step 6f - Define the output layer
-    model.add(layers.Dense(num_classes, activation='softmax'))
+    model.add(layers.Dense(units=num_classes, activation='softmax'))
 
     #TODO - Application 1 - Step 6g - Compile the model
-    model.compile(loss='categorical_crossentropy', optimizer='SGD', metrics=['accuracy'])
+    opt = SGD(learning_rate=0.001, momentum=0.9)
+    model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
 
 
     return model
@@ -104,11 +107,11 @@ def defineTrainAndEvaluateClassic(trainX, trainY, testX, testY):
     model = defineModel(input_shape, num_classes)
 
     #TODO - Application 1 - Step 7 - Train the model
-    model.fit(trainX,trainY,validation_data=(trainX,trainY),epochs=5,batch_size=32,verbose=2)
+    model.fit(x=trainX,y=trainY,validation_data=(trainX,trainY),epochs=5,batch_size=32,verbose=2,shuffle=True)
 
     #TODO - Application 1 - Step 8 - Evaluate the model
-    model.evaluate(testX, testY, verbose=1)
-
+    scores = model.evaluate(x=testX, y=testY, verbose=2)
+    print("Model1 Error: {:.2f}".format(100 - scores[1] * 100))
 
     return
 #####################################################################################################################
@@ -120,33 +123,34 @@ def defineTrainAndEvaluateClassic(trainX, trainY, testX, testY):
 def defineTrainAndEvaluateKFolds(trainX, trainY, testX, testY):
 
     k_folds = 5
-
+    input_shape = trainX.shape[1:]
+    num_classes = trainY.shape[1]
     accuracyScores = []
     histories = []
 
     #Application 2 - Step 2 - Prepare the cross validation datasets
-    kfold = KFold(k_folds, shuffle=True, random_state=1)
-
+    kfold = KFold(n_splits=k_folds, shuffle=True, random_state=1)
     for train_idx, val_idx in kfold.split(trainX):
-
         #TODO - Application 2 - Step 3 - Select data for train and validation
-
+        trainX_i = trainX[train_idx]
+        trainY_i = trainY[train_idx]
+        valX_i = trainX[val_idx]
+        valY_i = trainY[val_idx]
 
         #TODO - Application 2 - Step 4 - Build the model - Call the defineModel function
-
+        model = defineModel(input_shape, num_classes)
 
         #TODO - Application 2 - Step 5 - Fit the model
-
-
+        history = model.fit(x=trainX_i, y=trainY_i, epochs=5, batch_size=32, validation_data=(valX_i, valY_i), verbose=1)
+        print("sal")
         #TODO - Application 2 - Step 6 - Save the training related information in the histories list
-
+        histories.append(history)
 
         #TODO - Application 2 - Step 7 - Evaluate the model on the test dataset
-
+        scores = model.evaluate(x=testX, y=testY, verbose=2)
 
         #TODO - Application 2 - Step 8 - Save the accuracy in the accuracyScores list
-
-        pass #DELETE THIS!
+        accuracyScores.append(scores)
 
     return histories, accuracyScores
 #####################################################################################################################
@@ -163,15 +167,28 @@ def main():
 
     #TODO - Application 1 - Step 3 - Print the size of the train/test dataset
     print(f"Date train {trainX.shape} si date test {testX.shape}")
+    # Creăm o figură pentru a vizualiza primele 9 imagini
+    fig, axes = pyplot.subplots(3, 3, figsize=(8, 8))
+
+    # Afișăm primele 9 imagini
+    for i in range(9):
+        ax = axes[i // 3, i % 3]
+        ax.imshow(trainX[i], cmap='gray')  # Afișează imaginea la indexul i
+        ax.axis('off')  # Ascunde axele pentru o prezentare mai curată
+        ax.set_title(f'Label: {trainY[i]}')  # Afișează eticheta clasei pentru fiecare imagine
+
+    pyplot.tight_layout()  # Ajustează spațiul între subgrafice
+    pyplot.show()
 
     #TODO - Application 1 - Step 4 - Call the prepareData method
-    (trainX, trainY, testX, testY) = prepareData(trainX, trainY, testX, testY)
+    trainX, trainY, testX, testY = prepareData(trainX, trainY, testX, testY)
 
     #TODO - Application 1 - Step 5 - Define, train and evaluate the model in the classical way
-    model1 = defineTrainAndEvaluateClassic(trainX, trainY, testX, testY)
+    #model = defineTrainAndEvaluateClassic(trainX, trainY, testX, testY)
 
     #TODO - Application 2 - Step 1 - Define, train and evaluate the model using K-Folds strategy
     histories, accuracies = defineTrainAndEvaluateKFolds(trainX, trainY, testX, testY)
+    print(histories, accuracies)
 
     #TODO - Application 2 - Step9 - System performance presentation
     summarizeLearningCurvesPerformances(histories, accuracies)
